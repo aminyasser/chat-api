67935994dc643ab7231c3eebf2a6354c88b9d03c
aminyasser <aminyasser88@gmail.com>
2023-12-19 05:34:04 +0200

    remove .env from gitigonre

--- rails-service/entrypoint.sh
+++ rails-service/entrypoint.sh
@@ -1,5 +1,6 @@
 #  bundle exec rake install
 rm -f tmp/pids/server.pid
-bundle exec rake elasticsearch:create_index
+bundle exec rails db:create
 bundle exec rails db:migrate
+bundle exec rake elasticsearch:create_index
 bundle exec rails server -b 0.0.0.0
